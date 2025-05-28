e402f2f729d953763cb1b32df12d98a2db266aa0
mivalt.filip <mivalt.filip@gmail.com>
2025-05-28 15:33:16 -0500

    Refactor

---
+++ README.md
@@ -0,0 +1,23 @@
+# BrainMaze: Brain Electrophysiology, Behavior and Dynamics Analysis Toolbox - Data Files
+
+This toolbox provides data conversion tools for the BrainMaze project.
+
+## Installation
+
+```bash
+pip install --force git+https://github.com/bnelair/brainmaze_data_files.git@fmivalt/dev-cadence_ds
+```
+
+## License
+This software is licensed under BSD-3Clause license. For details see the `LICENSE <https://github.com/bnelair/brainmaze_utils/blob/master/LICENSE>`_ file in the root directory of this project.
+
+
+## Acknowledgment
+
+This code was developed and originally published for the first time with by (Mivalt 2022, and Sladky 2022).
+We appreciate you citing these papers when utilizing this toolbox in further research work.
+
+- F. Mivalt et V. Kremen et al., “Electrical brain stimulation and continuous behavioral state tracking in ambulatory humans,” J. Neural Eng., vol. 19, no. 1, p. 016019, Feb. 2022, doi: 10.1088/1741-2552/ac4bfd.
+- V. Sladky et al., “Distributed brain co-processor for tracking spikes, seizures and behaviour during electrical brain stimulation,” Brain Commun., vol. 4, no. 3, May 2022, doi: 10.1093/braincomms/fcac115.
+
+
--- brainmaze_datutils/data_conversions.py
+++ brainmaze_datutils/data_conversions.py
@@ -7,8 +7,17 @@ import shutil
 from mef_tools.io import MefWriter, MefReader
 from scipy import signal
 from tqdm import tqdm
+from typing import List
 
-def downsample_cadence_mef3_to_250Hz(path_source, path_dest=None, time_step=10*60):
+
+def mef3_downsample_8khz_to_250Hz(
+        path_source: str,
+        path_dest: str=None,
+        time_step: float=10*60,
+        reference_ch: str=None,
+        channels_include: List[str]=[],
+        channels_ignore: List[str]=[]
+):
     fs_target = 250
 
     if not os.path.exists(path_source):
@@ -32,6 +41,9 @@ def downsample_cadence_mef3_to_250Hz(path_source, path_dest=None, time_step=10*6
     rdr = MefReader(path_source)
     fs = rdr.get_property('fsamp')
 
+    if not channels_include.__len__():
+        channels_include = rdr.channels
+
     print('#########################')
     print(f'Session {path_source}')
     print(f'Sample rate {max(fs)}')
@@ -51,13 +63,17 @@ def downsample_cadence_mef3_to_250Hz(path_source, path_dest=None, time_step=10*6
         s_ = ts
         e_ = s_ + (time_step * 1e6)
 
-        x_ref = rdr.get_data('REF', s_, e_)
+        x_ref = 0
+        if reference_ch is not None:
+            if reference_ch not in rdr.channels:
+                raise ValueError(f"Reference channel '{reference_ch}' not found in the MEF3 file.")
+            x_ref = rdr.get_data(reference_ch, s_, e_)
 
-        for ch in rdr.channels:
-            if 'status' in ch:
+        for ch in channels_include:
+            if 'ch' in channels_ignore:
                 continue
 
-            if 'REF' in ch:
+            if reference_ch in ch:
                 continue
 
             fs = rdr.get_property('fsamp', ch)
@@ -96,7 +112,63 @@ def downsample_cadence_mef3_to_250Hz(path_source, path_dest=None, time_step=10*6
             if os.path.exists(file_annot_src):
                 shutil.copyfile(file_annot_src, file_annot_dest)
 
-def crop_cadence_mef3(path_source, path_dest=None, start=None, end=None, re_reference=True):
+def mef3_crop(
+        path_source: str,
+        path_dest: str=None,
+        start_utc: float=None,
+        end_utc: float=None,
+        reference_ch: str=None,
+        channels_include: List[str] = [],
+        channels_ignore:List[str] = []
+):
+    """
+    Crops a MEF3 file to a specified time range and saves the result to a new file.
+
+    Parameters:
+    ----------
+    path_source : str
+        Path to the source MEF3 file. Must end with '.mefd'.
+    path_dest : str, optional
+        Path to save the cropped MEF3 file. If None, the output file will be saved
+        in the same directory as the source file with '_cropped' appended to the name.
+    start_utc : float
+        Start time for cropping in UTC seconds. Must be within the time range of the source file.
+    end_utc : float
+        End time for cropping in UTC seconds. Must be within the time range of the source file.
+    reference_ch : str, optional
+        Name of the reference channel to subtract from all other channels. If None, no reference subtraction is applied.
+    channels_include : list of str, optional
+        List of channel names to include in the cropped file. If empty, all channels are included.
+    channels_ignore : list of str, optional
+        List of channel names to exclude from the cropped file. These channels will be ignored even if they are in `channels_include`.
+
+    Raises:
+    -------
+    FileNotFoundError
+        If the source file does not exist.
+    ValueError
+        If the source file is not a valid MEF3 file or if the specified time range is outside the file's time range.
+        Also raised if the reference channel is not found in the file.
+
+    Notes:
+    ------
+    - The function creates a new MEF3 file with cropped data.
+    - If a reference channel is provided, its data is subtracted from all other channels before writing.
+    - Channels with 'status' in their name or those explicitly ignored are skipped.
+
+    Example:
+    --------
+    crop_mef3(
+        path_source="data/session1.mefd",
+        path_dest="data/session1_cropped.mefd",
+        start_utc=1620000000,
+        end_utc=1620003600,
+        reference_ch="REF",
+        channels_include=["CH1", "CH2"],
+        channels_ignore=["CH3"]
+    )
+    """
+
     if not os.path.exists(path_source):
         raise FileNotFoundError(f"Source file {path_source} does not exist.")
 
@@ -126,7 +198,7 @@ def crop_cadence_mef3(path_source, path_dest=None, start=None, end=None, re_refe
     start_file = min(rdr.get_property('start_time')) / 1e6
     end_file = max(rdr.get_property('end_time')) / 1e6
 
-    if not (start >= start_file and end <= end_file):
+    if not (start_utc >= start_file and end_utc <= end_file):
         raise ValueError(f"Start and end times must be within the file's time range: {start_file} - {end_file}")
 
     Wrt = MefWriter(path_new, overwrite=True)
@@ -135,25 +207,30 @@ def crop_cadence_mef3(path_source, path_dest=None, start=None, end=None, re_refe
     Wrt.section3_dict['GMT_offset'] = 0
     Wrt.mef_block_len = 10*8000
 
-    s_ = start * 1e6
-    e_ = end * 1e6
+    s_ = start_utc * 1e6
+    e_ = end_utc * 1e6
 
     x_ref = 0
-    if re_reference:
-        x_ref = rdr.get_data('REF', s_, e_)
+    if reference_ch is not None:
+        if reference_ch not in rdr.channels:
+            raise ValueError(f"Reference channel '{reference_ch}' not found in the MEF3 file.")
 
-    for ch in rdr.channels:
+        x_ref = rdr.get_data(reference_ch, s_, e_)
+
+    if channels_include.__len__() == 0:
+        channels_include = rdr.channels
+
+    for ch in channels_include:
         if 'status' in ch:
             continue
 
-        if 'REF' in ch and re_reference:
+        if ch == reference_ch:
+            continue
+
+        if ch in channels_ignore:
             continue
 
         fs = rdr.get_property('fsamp', ch)
-
-        if fs != 8000:
-            print(f'Channel {ch} has sample rate {fs}, skipping...')
-            continue
 
         unit = rdr.get_property('unit', ch)
         ufact = rdr.get_property('ufact', ch)
